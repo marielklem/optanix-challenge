cbda3d5dd2a12669cc5ed9d1f4640e910fd8e59e
marielklem <mariel.steinbeiser@gmail.com>
2019-05-21 12:23:57 -0400

    removed models

--- models/LetterCount.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const LetterSchema = new Schema({
-  key2 : {}
-})
-
-module.exports = mongoose.model('LetterCount', LetterSchema)
--- models/WordCount.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const WordSchema = new Schema({
-  key1 : {}
-})
-
-module.exports = mongoose.model('WordCount', WordSchema)
